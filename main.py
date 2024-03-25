90d803d82360e6af9ad1bde43faa6db2e3bd246f
aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>
2024-03-24 22:27:34 -0700

    alot of work
    Signed-off-by: aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 
+import os
 from typing import List
 import re
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 """
 ## OpenLRN
@@ -15,7 +17,7 @@ Lexical Bucketing:
         a. length
         b. # of words
         c. # of sentences
-        d. aggregate of sentence statistics:
+        d. process of sentence statistics:
             i. average difficulty
             ii. average uncommeness (is it in 1000 most common words?)
             iii. average disparetness (# of different words)
@@ -66,7 +68,7 @@ def serialise_common_words(filepath: str) -> pd.DataFrame:
     return LEX_TABLE
 
 
-def pipe(filepath: str) -> pd.DataFrame:
+def pipe(filepath: str) -> tuple[pd.DataFrame, float]:
     sentences: List[str] = []
     count: int = 0
     word_count: int = 0
@@ -93,19 +95,46 @@ def pipe(filepath: str) -> pd.DataFrame:
         lambda sentence: len(set(sentence.split())))
     LEX_TABLE["uncommon"] = LEX_TABLE["sentence"].apply(
         lambda sentence: sum(word not in common for word in sentence.split()))
-    LEX_TABLE["distribution"] = LEX_TABLE["count"].apply(
+    LEX_TABLE["weight"] = LEX_TABLE["count"].apply(
         lambda c: (c / LEX_TABLE["count"].sum()))
+    LEX_TABLE["uniqueness"] = LEX_TABLE.apply(
+        lambda row: row["unique"] / row["count"], axis=1)
     LEX_TABLE["rarity"] = LEX_TABLE.apply(
         lambda row: row["uncommon"] / row["count"], axis=1)
     LEX_TABLE["verbosity"] = LEX_TABLE.apply(
         lambda row:  nverbose(row["sentence"], normal) / row["count"], axis=1)
+    LEX_TABLE["rigor"] = LEX_TABLE.apply(
+        lambda row: (row["uniqueness"] + row["rarity"] +
+                     row["verbosity"] / 3) * row["weight"],
+        axis=1)
+    LEX_TABLE["crigor"] = LEX_TABLE["rigor"].cumsum()
     LEX_TABLE["nlens"] = LEX_TABLE["sentence"].apply(nlen, normal=normal)
 
-    return LEX_TABLE
+    filename = filepath.rsplit('/')[-1]
+    LEX_TABLE.to_html(f"./out/{filename}.html")
+    LEX_TABLE.to_csv(f"./out/{filename}.csv")
+    plot(filename, LEX_TABLE)
+    return LEX_TABLE, LEX_TABLE['rigor'].sum()
 
 
 def nlen(sentence: str, normal: float) -> list[float]:
     return [len(word) / normal for word in sentence.split()]
+
+
+def plot(filename: str, table: pd.DataFrame) -> None:
+    fig, axis = plt.subplots(1, 2, figsize=(
+        10, 5), gridspec_kw={'width_ratios': [2, 0]})
+    axis[0].scatter(table.index, table["crigor"])
+    axis[0].grid(True)
+    axis[0].set_ylabel('Cumulative Rigor')
+    # axis[1].scatter(table.index, table["weight"])
+    # axis[1].grid(True)
+    # axis[1].set_ylabel('Weight')
+    plt.suptitle(
+        f"Rigor analysis of {filename}\nTotal Rigor : {round(table['rigor'].sum(), 4)}", ha='center')
+    plt.xlabel("Sentence Coordinate")
+    plt.tight_layout()
+    plt.savefig(f"./out/{filename}.png")
 
 
 def nverbose(sentence: str, normal: float) -> float:
@@ -117,4 +146,13 @@ def nverbose(sentence: str, normal: float) -> float:
     return count
 
 
-(pipe("./resources/ur-facism.txt"))
+def process(d: str) -> pd.DataFrame:
+    rigors = {}
+    for filename in os.listdir(d):
+        _, rigor = pipe(os.path.join(d, filename))
+        rigors[filename] = rigor
+
+    return pd.DataFrame(list(rigors.items()), columns=["Text", "Rigor"])
+
+
+print(process("./resources"))
